a8fc272784ba5eb43848f6d416282b0f79613525
Roman Maksymiuk <romanmaksymiuk1@gmail.com>
2022-10-31 20:57:48 +0100

    added tests

--- Tests.py
+++ Tests.py
@@ -1,14 +1,41 @@
 import unittest
+from User import User
+from Database import Database
 
 class Tests(unittest.TestCase):
     def testUser(self):
-        return
+        user = User('Roman123', 'password')
+        assert user.login('Roman123', 'password') is True
+        assert user.login('ROman123', 'password') is False
 
-    def testApplication(self):
-        return
+    def testUserFromDatabase(self):
+        database = Database()
+        database = Database()
+        database.loadDatabase()
+        database.loadPermissions()
 
-    def testDatabase(self):
-        return
+        assert len(database.fileList) == 4
+        assert len(database.userList) == 4
 
-    def testHub(self):
-        return
+    def testUserFromDatabase2(self):
+        database = Database()
+        database = Database()
+        database.loadDatabase()
+        database.loadPermissions()
+
+        assert database.userList.get('Bob123') == '123qwe'
+        assert database.userList.get('Tim1000') == '12345qwert'
+        assert database.userList.get('John333') == '123456qwerty'
+        assert database.userList.get('Grant111') == '1234qwer'
+
+    def testUserFromDataBase3(self):
+        database = Database()
+        database = Database()
+        database.loadDatabase()
+        database.loadPermissions()
+
+        assert len(database.permissions) == 4
+        assert isinstance(database.permissions.get('Bob123')[1], list) == True
+
+    # def testHub(self):
+    #     return
