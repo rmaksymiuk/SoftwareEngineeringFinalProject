46f6b5124ded6b20bb24ae7133f26cd661aab126
jingyihu <jingyihu@kth.se>
2022-10-31 20:08:53 +0100

    Finish GUI

--- EditForm.py
+++ EditForm.py
@@ -1,25 +1,30 @@
 from tkinter import *
 import os , sys, subprocess, fnmatch
 
-# Create Interface for
-root = Tk()
-root.title('Search for your form')
-canvas1 = Canvas(root, width=400, height=300)
-canvas1.pack()
-entry1 = Entry(root)
-canvas1.create_window(200, 140, window=entry1)
 
-def GetCaseID():
-    InputCaseID = entry1.get()
-    for file in os.listdir('Applications/'):
-        if fnmatch.fnmatch(file, '*' + str(InputCaseID) + '.xlsx'):
-            print(file)
-            file = 'Applications/' + file
-            opener = "open" if sys.platform == "darwin" else "xdg-open"
-            subprocess.call([opener, file])
+class SearchFile(Tk):
+    def __init__(self):
+        super().__init__()
 
+        self.title('Search for your form')
+        canvas1 = Canvas(self, width=400, height=300)
+        canvas1.pack()
+        entry1 = Entry(self)
+        canvas1.create_window(200, 140, window=entry1)
 
-button1 = Button(text='Search for the Case ID', command=GetCaseID)
-canvas1.create_window(200, 180, window=button1)
+        def GetCaseID():
+            InputCaseID = entry1.get()
+            for file in os.listdir('Applications/'):
+                if fnmatch.fnmatch(file, '*' + str(InputCaseID) + '.xlsx'):
+                    print(file)
+                    file = 'Applications/' + file
+                    opener = "open" if sys.platform == "darwin" else "xdg-open"
+                    subprocess.call([opener, file])
 
-root.mainloop()
+        button1 = Button(self, text='Search for the Case ID', command=GetCaseID)
+        canvas1.create_window(200, 180, window=button1)
+
+if __name__ == "__main__":
+    root = SearchFile()
+    root.mainloop()
+
